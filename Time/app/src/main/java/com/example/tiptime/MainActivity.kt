cc2c85d2f28944c49dfcc58504e798f267e11bd4
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-21 18:21:34 -0500

    Change of icon for the app visualization

--- Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.tiptime
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
@@ -32,39 +33,11 @@ class MainActivity : AppCompatActivity() {
 
         val tipCalculator = TipCalculator(cost, tipPercentage, roundUp)
         val tip = tipCalculator.calculateTip()
+        // Aqui estoy llamando el metodo de la instancia tipcalculater que retorna un String
+        // y lo almaceno en la variable String llamada tip
 
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        binding.tipResult.text = getString(R.string.tip_amount, tip)
 
     }
 
 }
-
-
-//    fun calculateTip() {
-//
-//        val stringIntTextField = binding.costOfService.text.toString()
-//        val cost = stringIntTextField.toDoubleOrNull()
-//
-//        if (cost == null) {
-//            binding.tipResult.text = ""
-//            return
-//        }
-//
-//        val selectedId = binding.tipOptions.checkedRadioButtonId
-//        val tipPercentage = when (selectedId) {
-//            R.id.option_twenty_percent -> 0.20
-//            R.id.option_eighteen_percent -> 0.18
-//            else -> 0.15
-//        }
-//
-//        var tip = tipPercentage * cost
-//        val roundUp = binding.roundUpSwitch.isChecked
-//        if (roundUp) {
-//            tip = kotlin.math.ceil(tip)
-//        }
-//
-//        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-//        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-//
-//    }
--- Time/app/src/main/java/com/example/tiptime/TipCalculator.kt
+++ Time/app/src/main/java/com/example/tiptime/TipCalculator.kt
@@ -4,7 +4,12 @@ import java.text.NumberFormat
 
 class TipCalculator(val costOfService: Double?, val percentageTip: Double, val roundTip: Boolean) {
 
+   val estaVariableSePuedeAccederEnTodosLosMetodosyDesdeAfueraConUnaInstancia: Int = 0
+   private val estaVariableSePuedeAccederEnTodosLosMetodosPeroNoDesdeAfuera: Int = 0
+
    fun calculateTip(): String {
+      // Este metodo retorna un String que se llama variableFormateadaDeTipQueVoyARetornarPorElMetodo
+      // pero no importa porque simepre return String
 
       val cost = costOfService
 
@@ -17,8 +22,16 @@ class TipCalculator(val costOfService: Double?, val percentageTip: Double, val r
          tip = kotlin.math.ceil(tip)
       }
 
-      val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-      return formattedTip
+      val variableFormateadaDeTipQueVoyARetornarPorElMetodo = NumberFormat.getCurrencyInstance().format(tip)
+      return variableFormateadaDeTipQueVoyARetornarPorElMetodo
+
+   }
+
+   fun otrometodo() {
+      // YO NO CONOZCO NINGUNA DE LAS VARIABLES QUE ESTA
+      // EN EL METODO CALCULATETIP
+
+      // YO SI CONOZCO TODAS LAS VARIABLES QUE ESTAN A NIVEL DE CLASE
 
    }
 
