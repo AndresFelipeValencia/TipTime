d699d9efb8c23cd83e530ed2cbcbb6f8620afc59
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-11-22 21:26:04 -0500

    app completed

--- Time/app/src/main/java/com/example/tiptime/TipCalculator.kt
+++ Time/app/src/main/java/com/example/tiptime/TipCalculator.kt
@@ -8,21 +8,22 @@ import java.text.NumberFormat
 
 class TipCalculator(val costOfService: Double?, val percentageTip: Double, val roundTip: Boolean) {
 
-   fun calculateTip(): String {
-      // Este metodo retorna un String que se llama variableFormateadaDeTipQueVoyARetornarPorElMetodo
-      // pero no importa porque simepre return String
+    fun calculateTip(): String {
+        // Este metodo retorna un String que se llama variableFormateadaDeTipQueVoyARetornarPorElMetodo
+        // pero no importa porque simepre return String
 
-      val cost = costOfService ?: return "0.0"
+        val cost = costOfService ?: return "0.0"
 
-      var tip = percentageTip * cost
-      if (roundTip) {
-         tip = kotlin.math.ceil(tip)
-      }
+        var tip = percentageTip * cost
+        if (roundTip) {
+            tip = kotlin.math.ceil(tip)
+        }
 
-      val variableFormateadaDeTipQueVoyARetornarPorElMetodo = NumberFormat.getCurrencyInstance().format(tip)
-      return variableFormateadaDeTipQueVoyARetornarPorElMetodo
+        val variableFormateadaDeTipQueVoyARetornarPorElMetodo =
+            NumberFormat.getCurrencyInstance().format(tip)
+        return variableFormateadaDeTipQueVoyARetornarPorElMetodo
 
-   }
+    }
 
 
 }
